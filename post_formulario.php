09bf8965f2feeac02367e801d94437860ed54867
JoaoVitor <joao.t@aluno.ifsp.edu.br>
2022-09-14 12:33:06 -0300

    pf

---
+++ post_formulario.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post | Projeto para Web com PHP</title>
+        <link rel="stylesheet"  href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="room">
+            <div class="col-md-12">
+                <!-- Topo //-->
+                <?php
+                    include 'includes/topo.php';
+                    include 'includes/valida_login.php';
+                ?>
+            </div>
+        </div>
+        <div class="row" style="min-height: 500px;">
+            <div class="col-md-12">
+                <?php include 'includes/menu.php'; ?>
+            </div>
+            <div class="col-md-10" style="padding-top: 50px;">
+                <?php
+                        require_once 'includes/funcoes.php';
+                        require_once 'core/conexao_mysql.php';
+                        require_once 'core/sql.php';
+                        require_once 'core/mysql.php';
+
+                        foreach($_GET as $indice => $dado) {
+                                $$indice = limparDados($dado);
+                        }
+
+                        if(!empty($id)) {
+                            $id = (int)$id;
+
+                            $criterio = [
+                                ['id', '=', $id]
+                            ];
+
+                            $retorno = buscar(
+                                'post',
+                                ['*'],
+                                $criterio
+                            );
+
+                            $entidade = $retorno[0];
+                        }
+                    ?>
+                    <h2>Post</h2>
+              <form method="post" action="core/post_repositorio.php">
+                <input type="hidden" name="acao"
+                        value="<?php echo empty($id) ? 'insert' : 'uptade' ?>"
+                <input type="hidden" name="id"
+                        value="<?php echo $entidade[$id] ?? ''?>">
+                <div class="form-group">
+                    <label for="titulo">Título</label>
+                    <input class="form-control" type="text"
+                    require="required" id="titulo" name="titulo"
+                    value="<?php echo $entidade['título'] ?? '' ?>">
+                </div>
+                <div class="form-group">
+                    <label for="texto">Texto</label>
+                    <textarea class="form-control" type="text"
+                        require="required" id="texto" name="texto" rows="5">
+                        <?php echo $entidade['texto'] ?? '' ?>
+                    </textarea>
+                </div>
+
+
+                        
